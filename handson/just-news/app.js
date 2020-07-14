9c52b87a52cc7b3d19eae7c53676af14ec93d7a6
Muhun Kim <iam@muhun.kim>
2020-07-14 22:01:11 +0900

    여러 사이트에 적용하기 전의 준비 작업
    공통 관심사를 함수로 묶음

--- handson/just-news/app.js
+++ handson/just-news/app.js
@@ -13,19 +13,14 @@ function optOutUrl() {
   return window.location.origin + window.location.pathname + query
 }
 
-if (!isOptOut()) {
-  const title = document.querySelector('#main > h1').cloneNode(true)
-  const timeStemp = document.querySelector('.date').cloneNode(true)
-  const repoter = document.querySelector('.user').cloneNode(true)
-  const content = document.querySelector('.pf-content').cloneNode(true)
-
+function reconstruct(articleInfo) {
   const root = document.createElement('html')
   document.replaceChild(root, document.documentElement)
   root.innerHTML = [
     '<head>',
     '<meta charset="utf-8">',
     '<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">',
-    `<title>${title}</title>`,
+    `<title>${articleInfo.title}</title>`,
     '</head>',
     '<body>',
     '<article>',
@@ -33,14 +28,25 @@ if (!isOptOut()) {
     '</body>',
   ].join('')
 
-  const article = document.querySelector('article')
+  const article = root.querySelector('article')
 
-  article.append(content)
-  article.insertAdjacentElement('afterbegin', repoter)
-  article.insertAdjacentElement('afterbegin', timeStemp)
-  article.insertAdjacentElement('afterbegin', title)
+  article.append(articleInfo.content)
+  article.insertAdjacentElement('afterbegin', articleInfo.repoter)
+  article.insertAdjacentElement('afterbegin', articleInfo.timeStemp)
+  article.insertAdjacentElement('afterbegin', articleInfo.title)
   article.insertAdjacentHTML(
     'afterbegin',
     `<a href=${optOutUrl()}>원본 페이지 보기</a>`
   )
+}
+
+const articleInfo = {
+  title: document.querySelector('#main > h1').cloneNode(true),
+  timeStemp: document.querySelector('.date').cloneNode(true),
+  repoter: document.querySelector('.user').cloneNode(true),
+  content: document.querySelector('.pf-content').cloneNode(true),
+}
+
+if (!isOptOut()) {
+  reconstruct(articleInfo)
 }
