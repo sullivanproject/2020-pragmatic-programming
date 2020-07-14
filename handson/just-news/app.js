a73001bc028901169e0f26c120fe4f52416271b3
Muhun Kim <iam@muhun.kim>
2020-07-14 21:50:24 +0900

    옵트 아웃 기능 구현
    문서가 다 로딩된 뒤에 스크립트를 실행하기로 매니패스트 설정을 수정함
    ref: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/content_scripts

--- handson/just-news/app.js
+++ handson/just-news/app.js
@@ -1,25 +1,46 @@
-const title = document.querySelector('#main > h1').cloneNode(true)
-const timeStemp = document.querySelector('.date').cloneNode(true)
-const repoter = document.querySelector('.user').cloneNode(true)
-const content = document.querySelector('.pf-content').cloneNode(true)
+const optOutQueryKey = 'just_news'
 
-const root = document.createElement('html')
-document.replaceChild(root, document.documentElement)
-root.innerHTML = [
-  '<head>',
-  '<meta charset="utf-8">',
-  '<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">',
-  `<title>${title}</title>`,
-  '</head>',
-  '<body>',
-  '<article>',
-  '</article>',
-  '</body>',
-].join('')
+function isOptOut() {
+  return window.location.search.indexOf(`${optOutQueryKey}=false`) !== -1
+}
+function optOutUrl() {
+  let query = window.location.search
+  if (query) {
+    query += `&${optOutQueryKey}=false`
+  } else {
+    query = `?${optOutQueryKey}=false`
+  }
+  return window.location.origin + window.location.pathname + query
+}
 
-const article = document.querySelector('article')
+if (!isOptOut()) {
+  const title = document.querySelector('#main > h1').cloneNode(true)
+  const timeStemp = document.querySelector('.date').cloneNode(true)
+  const repoter = document.querySelector('.user').cloneNode(true)
+  const content = document.querySelector('.pf-content').cloneNode(true)
 
-article.append(content)
-article.insertAdjacentElement('afterbegin', repoter)
-article.insertAdjacentElement('afterbegin', timeStemp)
-article.insertAdjacentElement('afterbegin', title)
+  const root = document.createElement('html')
+  document.replaceChild(root, document.documentElement)
+  root.innerHTML = [
+    '<head>',
+    '<meta charset="utf-8">',
+    '<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">',
+    `<title>${title}</title>`,
+    '</head>',
+    '<body>',
+    '<article>',
+    '</article>',
+    '</body>',
+  ].join('')
+
+  const article = document.querySelector('article')
+
+  article.append(content)
+  article.insertAdjacentElement('afterbegin', repoter)
+  article.insertAdjacentElement('afterbegin', timeStemp)
+  article.insertAdjacentElement('afterbegin', title)
+  article.insertAdjacentHTML(
+    'afterbegin',
+    `<a href=${optOutUrl()}>원본 페이지 보기</a>`
+  )
+}
